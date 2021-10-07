55c4f0a641cef088d190067448f682fa0752d0d8
Popopz <mpcmurphyait@gmail.com>
2021-10-07 17:32:37 +1000

    Installed Mongoose

--- server/server.js
+++ server/server.js
@@ -21,3 +21,18 @@ server.listen(http,PORT);
 //app.post('login', require('.router/postlogin.js'));
 //postlogin.js:
 //module.exports(req, res);
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/test');
+    //https://mongoosejs.com/docs/
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+    const Kitten = mongoose.model('Kitten', kittySchema);
+    const silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence'
+    await silence.save();
+};
--- server/sockets.js
+++ server/sockets.js
@@ -1,6 +1,7 @@
 module.exports = {
     connect: function(io, PORT){
         io.on('connection', (socket) => {
+            //socket.join("some room");
             console.log('sockets.js user connection on port ' + PORT + ' : ' + socket.id);
             socket.on('message', (message)=>{
                 io.emit('message', message);
