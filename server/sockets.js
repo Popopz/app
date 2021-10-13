ad69a625f52b6f4a70ca6d7046e8482bf2cf63ba
Popopz <mpcmurphyait@gmail.com>
2021-10-13 23:26:58 +1000

    Attempted to add Socket.JS user disconnect

--- server/sockets.js
+++ server/sockets.js
@@ -2,7 +2,7 @@ module.exports = {
     connect: function(io, PORT){
         io.on('connection', (socket) => {
             socket.on('disconnect', ()=>{
-                users
+                io.emit('message', "User Left");
             })
             io.emit('message','User Has joined');
             console.log('sockets.js user connection on port ' + PORT + ' : ' + socket.id);
